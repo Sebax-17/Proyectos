5866eb287c8fd1b1d2b960a838d0c0799bdd293e
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-02-17 08:45:24 -0500

    Ultime

--- Program.js
+++ Program.js
@@ -2,10 +2,10 @@ const readlineSync = require('readline-sync');
 
 const cantidadProductos = +readlineSync.question('Ingrese la cantidad de productos: ');
 
-let costoTotalFlete = 0;
-let VolumenMayor = 0;
-let impuesto = 0;
-let impuestoTotalFlete = 0;
+let TotalFlete = 0;
+let DimencionMayor = 0;
+let Imp = 0;
+let ImpTotalFlete = 0;
 
 for (let i = 0; i < cantidadProductos; i++) {
     const numeroEmpaque = readlineSync.question(`Ingrese el número del empaque ${i + 1}: `);
@@ -15,54 +15,54 @@ for (let i = 0; i < cantidadProductos; i++) {
         continue;
     }
 
-    const anchoProducto = +readlineSync.question(`Ingrese el ancho del producto ${i + 1}: `);
-    if (anchoProducto < 0) {
+    const ancho = +readlineSync.question(`Ingrese el ancho del producto ${i + 1}: `);
+    if (ancho < 0) {
         console.error("El ancho del producto no es válido");
         continue; 
     }
 
-    const altoProducto = +readlineSync.question(`Ingrese el alto del producto ${i + 1}: `);
-    if (altoProducto < 0) {
+    const alto = +readlineSync.question(`Ingrese el alto del producto ${i + 1}: `);
+    if (alto < 0) {
         console.error("El alto del producto no es válido");
         continue;
     }
 
-    const profundidadProducto = +readlineSync.question(`Ingrese la profundidad del producto ${i + 1}: `);
-    if (profundidadProducto < 0) {
+    const profundidad = +readlineSync.question(`Ingrese la profundidad del producto ${i + 1}: `);
+    if (profundidad < 0) {
         console.error("La profundidad del producto no es válida");
         continue;
     }
 
-    const volumen = (anchoProducto * altoProducto * profundidadProducto)*100;
+    const volumen = (ancho * alto * profundidad)*100;
 
    
     
     if (volumen > 1000 && volumen < 10000) {
-        impuesto = volumen * 0.1;
+        Imp = volumen * 0.1;
     } 
        else if (volumen > 10000) {
-         impuesto = volumen * 0.2;
+         Imp = volumen * 0.2;
     }
 
    
-    const costoTotalProducto = (volumen + impuesto) ;
+    const costoTotalProducto = (volumen + Imp) ;
 
-    costoTotalFlete += costoTotalProducto;
+    TotalFlete += costoTotalProducto;
 
     
-    if (volumen > VolumenMayor) {
-        mayorVolumen = volumen; 
+    if (volumen > DimencionMayor) {
+        DimencionMayor = volumen; 
     }
 
-    impuestoTotalFlete += impuesto;
+    ImpTotalFlete += Imp;
 }
 
 
 
 
- const promediodelflete = costoTotalFlete/cantidadProductos;
+ const promediodelflete = TotalFlete/cantidadProductos;
 
-console.info("El costo total del flete es: ",{costoTotalFlete});
-console.info("Producto de mayores dimensiones: ",{mayorVolumen});
+console.info("El costo total del flete es: ",{TotalFlete});
+console.info("Producto de mayores dimensiones: ",{DimencionMayor});
 console.info("Promedio total del flete es: ",{promediodelflete});
-console.info("El impuesto total por el flete es: ",{impuestoTotalFlete});
+console.info("El impuesto total por el flete es: ",{ImpTotalFlete});
